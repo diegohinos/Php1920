77e5fee8cd2e0d5f4434c55cba0e6671aa6c093e
diegohinos <59961438+diegohinos@users.noreply.github.com>
2020-01-16 13:47:28 +0100

    Update empaltaemp1.php

--- empaltaemp1.php
+++ empaltaemp1.php
@@ -1,5 +1,4 @@
 <?php
-require "../../Funciones/Funciones.php";
 require "empaltaemp-funciones.php";
 $dni=$_POST['dni'];
 $nombre=$_POST['nombre'];
@@ -8,9 +7,7 @@ $fecha=$_POST['fecha'];
 $salario=$_POST['salario'];
 $dpto=$_POST['departamento'];
 $Empleado=array($dni,$nombre,$apellido,$fecha,$salario,$dpto);
-for ($i=0; $i < count($Empleado); $i++) { 
-    $Empleado[$i]=limpiar_campos($Empleado[$i]);
-}
-insertar("localhost","root","rootroot","empleadonn",$Empleado);
+
+insertar("10.129.6.54","root","rootroot","empleadonn",$Empleado);
 
 ?>
